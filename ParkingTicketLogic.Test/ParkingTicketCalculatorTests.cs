ece8c3472f10348c6b74f899e3a40772e702a7b2
Larry Gasik <larry@lgproductions.us>
2019-06-06 01:03:26 -0500

    Wrote tests for issuing a ticket on a holiday based on the date. Abstracted out datetime functionality.

---
+++ ParkingTicketLogic.Test/ParkingTicketCalculatorTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using ParkingTicket.DataAccess;
+using ParkingTicket.DataAccess.DTO;
+using ParkingTicketLogic.DTO;
+using ParkingTicketLogic.Providers;
+
+namespace ParkingTicketLogic.Test
+{
+    [TestFixture]
+    public class ParkingTicketCalculatorTests
+    {
+        private ParkingTicketCalculator _sut;
+        private Mock<IHolidayService> _IHolidayService;
+
+        [SetUp]
+        public void SetUp()
+        {
+           _IHolidayService = new Mock<IHolidayService>();
+        }
+
+        [Test]
+        public void TicketsShouldNotBeIssuedWhenItIsAHoliday()
+        {
+            SystemTime.SetDateTime(new DateTime(2019,05,22));
+            _IHolidayService.Setup(x => x.GetHolidays())
+                .Returns(
+                    new List<HolidayDTO>
+                    {
+                        new HolidayDTO{Date = new DateTime(2019,05,22),TitleOfDay = "Mayor's Birthday"},
+                    });
+            _sut = new ParkingTicketCalculator(_IHolidayService.Object);
+            string result = _sut.ScanForOffense(new ScanInformation {Offense = ParkingOffense.ExpiredParkingMeter, Tag = "Bill"});
+            Assert.AreEqual(String.Empty, result);
+            
+        }
+
+        [Test]
+        public void TicketsShouldBeIssuedWhenItIsAHoliday()
+        {
+            SystemTime.SetDateTime(new DateTime(2019, 05, 21));
+            _IHolidayService.Setup(x => x.GetHolidays())
+                .Returns(
+                    new List<HolidayDTO>
+                    {
+                        new HolidayDTO{Date = new DateTime(2019,05,22),TitleOfDay = "Mayor's Birthday"},
+                    });
+            _sut = new ParkingTicketCalculator(_IHolidayService.Object);
+            string result = _sut.ScanForOffense(new ScanInformation { Offense = ParkingOffense.ExpiredParkingMeter, Tag = "Bill" });
+            Assert.AreEqual("here's your ticket", result);
+
+        }
+    }
+}
