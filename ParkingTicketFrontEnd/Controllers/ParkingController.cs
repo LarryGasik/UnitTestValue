1ec47aead4125048a1c5d68bb5a5cf11b65438cf
Edward Wisniowski <edward1968@yahoo.com>
2019-07-01 14:43:05 -0500

    added a the base schafolding of all the parking application front end.

---
+++ ParkingTicketFrontEnd/Controllers/ParkingController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ParkingTicketFrontEnd.Controllers
+{
+    public class ParkingController : Controller
+    {
+        /// <summary>
+        /// Use this to load the initial page
+        /// </summary>
+        /// <returns>Nothing this is a sample git</returns>
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
