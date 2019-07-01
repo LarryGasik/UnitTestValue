9497457cff0576c3c82c56c759fbd5036431ca51
Edward Wisniowski <edward1968@yahoo.com>
2019-07-01 15:29:34 -0500

    I have created a Parking controler and set up the basics of the form to check if someone is doing what they need to do.

--- ParkingTicketFrontEnd/Controllers/ParkingController.cs
+++ ParkingTicketFrontEnd/Controllers/ParkingController.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ParkingTicketLogic;
 
 namespace ParkingTicketFrontEnd.Controllers
 {
     public class ParkingController : Controller
     {
         /// <summary>
-        /// Use this to load the initial page
+        /// I ripped off this code from 
+        /// https://www.codeproject.com/Articles/1056011/Bind-Enum-to-DropdownList-in-ASP-NET-MVC
+        /// To create a quick and dirty means to pull enumerations into the UI 
+        /// without too much fuss or violating the DRY principle.
+        /// ejw 1-JLY-2019
         /// </summary>
         /// <returns>Nothing this is a sample git</returns>
         [HttpGet]
         public ActionResult Index()
         {
+           //a bit of a hack but this will work
+           ViewBag.OffenseList = from ParkingOffense e in Enum.GetValues(typeof(ParkingOffense))
+                                  select new
+                                  {
+                                      ID = (int)e,
+                                      Name = e.ToString()
+                                  };
+            //set up the form
             return View();
         }
     }
