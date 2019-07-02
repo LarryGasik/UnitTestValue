99d3b069446c69ee463455b3e0b19c6df74c7378
Edward Wisniowski <edward1968@yahoo.com>
2019-07-02 13:07:50 -0500

    I have a full working example of how the ticket system works.  This is a base version.  Future improvements include validation for the form and some Styling but otherwise the happy path works.

---
+++ ParkingTicketFrontEnd/Extensions/ParkingOffenseFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ParkingTicketLogic;
+
+namespace ParkingTicketFrontEnd.Extensions
+{
+    public class ParkingOffenseFactory
+    {
+        /// <summary>
+        /// welcome to the DRY example of not repeating yourself
+        /// write the code once so that you have one place to go
+        /// when something goes wrong. ejw 2-JLY-2019
+        /// I ripped off this code from 
+        /// https://www.codeproject.com/Articles/1056011/Bind-Enum-to-DropdownList-in-ASP-NET-MVC
+        /// To create a quick and dirty means to pull enumerations into the UI 
+        /// </summary>
+        /// <returns>MVC Select List</returns>
+        public static SelectList CreateOffenseSelectList()
+        {
+            SelectList _outbound;
+            
+            //a bit of a hack but this will work
+            var offfenses = from ParkingOffense e in Enum.GetValues(typeof(ParkingOffense))
+                                  select new
+                                  {
+                                      ID = (int)e,
+                                      Name = e.ToString()
+                                  };
+
+            _outbound = new SelectList(offfenses, "ID", "Name");
+
+            return _outbound;
+        }
+    }
+}
