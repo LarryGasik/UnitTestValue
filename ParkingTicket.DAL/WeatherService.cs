04f3dc7852d830b692197fededb0a93d86d5ee14
Larry Gasik <larry@lgproductions.us>
2024-06-13 12:00:22 -0500

    //Larry Messing With Turano

--- ParkingTicket.DAL/WeatherService.cs
+++ ParkingTicket.DAL/WeatherService.cs
@@ -8,5 +8,6 @@ public class WeatherService : IWeatherService
     {
         //This is just to simulate if it is snowing. It means nothing.
         return DateTime.Now.Second % 2 == 0;
+        //Larry Messing With Turano
     }
 }
